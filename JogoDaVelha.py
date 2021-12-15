10c0a4dcb03a9131637251a18763001f9c6dcf3d
Wesley <wesleyvitor37417@gmail.com>
2021-12-14 21:47:44 -0300

    Lógica basica do jogo

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -1,7 +1,9 @@
 from GerenciadorPalavras import *
-import io
+import os
 
 temas = temasDisponiveis()
+
+os.system('cls')
 
 print('Os temas disponíveis são:')
 
@@ -10,26 +12,47 @@ print('Os temas disponíveis são:')
 escolha = int(input('Digite sua escolha:\n'))
 
 palavra = sortearPalavra(temas[escolha])
-print(palavra)
+#print(palavra)
 
-palavraOculta = ['_ ' for _ in range(len(palavra))]
+#palavraOculta = [palavraOculta for _ in range(len(palavra))]
+palavraOculta = ['_ ' for i in range(len(palavra))]
+#palavraOculta = ''.join(palavraOcultaList)
+print(palavraOculta)
+tentativas = 6
 
+os.system('cls')
+print(''.join(palavraOculta))
 
 while True:
+    print('tentativas = ', tentativas)
     letra = input('letra:\n')
     letraPos = -1
+    letraCerta = False
 
+    os.system('cls')
 
     while True:
         letraPos= palavra.find(letra, letraPos+1)
-        print(letraPos)
 
         if(letraPos == -1):
             break
 
+        letraCerta = True
         palavraOculta[letraPos] = letra
 
+    if(not letraCerta):
+        tentativas -= 1
+        print('Você errou')
 
 
     [print(i, end = '') for i in palavraOculta]
     print('\n')
+
+    if(''.join(palavraOculta) == palavra):
+        print('Você ganhou :D')
+        break
+
+    if(tentativas <= 0):
+        print('Você perdeu :(')
+        print('A palavra certa era: ', palavra)
+        break
